e57ce90a50b6085af170a653b303315803c08fcf
evatatarenko <evatatarenko@yandex.ru>
2023-05-13 21:27:33 +0300

    last remarks satisfied

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 
 import Card from "../components/Card.js";
 import UserInfo from "../components/UserInfo.js";
-import FormValidator from "../components/Validator.js";
+import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
